5d247d0d145f23658dae6a17ea5ca1e73bce2954
mohaneswaran25 <137979134+mohaneswaran25@users.noreply.github.com>
2023-12-15 07:43:03 +0530

    Update kovil.py

--- kovil.py
+++ kovil.py
@@ -7,12 +7,10 @@ with col1:
    image = Image.open("image.jpg")
    new_image = image.resize((200, 250))
    st.image(new_image)
-   st.subheader("திரு சிவக்குமார்")
-   st.write("**திதி யோக கரண ஆராய்ச்சியாளர்**")
 with col2:
    st.header("திரு சிவக்குமார் திதி யோக கரண ஆராய்ச்சியாளர் ")
-   st.write("கால பைரவர் ஜோதிட பவனம் காடையாம்பட்டி சேலம்")
-   st.write("cell: +91 8883113734, +91 6379411673")
+   st.write("**கால பைரவர் ஜோதிட பவனம் காடையாம்பட்டி சேலம்**")
+   st.write("**cell: +91 8883113734, +91 6379411673**")
 
 
 #st.image("image.jpg",width=400,use_column_width=200)
